19e93f13b89dd03d802476d3b33f5f7bfc8ff1db
erwinagpasa <istran.net@gmail.com>
2021-12-14 20:11:48 +0300

    crud

--- bin/main.dart
+++ bin/main.dart
@@ -5,15 +5,15 @@ void main(List<String> arguments) async {
 
   await db.open(); //If you open the database make sure you close.
 
-  print('Connectec to database');
+  print('Connected to database');
 
   DbCollection coll = db.collection('people');
 
   //TODO: Read people
   //Full list of data
-  //var people = await coll.find().toList();
+  var people = await coll.find().toList();
   //Using find Where.eq (match check if the field contain set of character lik A)
-  var people = await coll.find(where.eq('first_name', 'Erwin')).toList();
+  //var people = await coll.find(where.eq('first_name', 'Erwin')).toList();
   //Using Limit
   //var people = await coll.find(where.limit(5)).toList();
   print(people);
@@ -32,7 +32,16 @@ void main(List<String> arguments) async {
   print('Data inserted');
 
   //TODO: Update person
+  await coll.update(await coll.findOne(where.eq('id', 3023)), {
+    r'$set': {'gender': 'Female'}
+  });
+  print('updated');
+  print(await coll.findOne(where.eq('id', 3023)));
 
   //TODO: Delete person
+  await coll.remove(await coll.findOne(where.eq('id', 3023)));
+  print(await coll.findOne(where.eq('id', 3023))); //retun null
+
+  //Close db connection
   await db.close();
 }
